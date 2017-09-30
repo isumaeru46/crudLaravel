031aa57e7e19dd84c64fdb10eceeefebadcd8c85
Ismael <you@example.com>
2017-09-30 09:21:38 -0300

    impl

--- resources/views/autocomplete/index.blade.php
+++ resources/views/autocomplete/index.blade.php
@@ -2,15 +2,30 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="col-xs-10 col-sm-10 col-md-10">
+            <div class="form-group">
+                {{ Form::hidden('nomeProduto', null, array('id' => 'nomeProduto_id')) }}
+                {!! Form::text('search_text', null, array('placeholder' => 'Procurar...','class' => 'form-control','id'=>'search_text')) !!}
+            </div>
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-2">
             <div class="form-group">
 
-                {!! Form::text('search_text', null, array('placeholder' => 'Search Text','class' => 'form-control','id'=>'search_text')) !!}
+                {!! Form::button('Adicionar',array('class' => 'btn','onclick'=>'adicionarProduto();')) !!}
             </div>
         </div>
     </div>
 
     <script>
+
+        function adicionarProduto(){
+            if(document.getElementById('nomeProduto_id').value != ""){
+                alert(document.getElementById('nomeProduto_id').value);
+                document.getElementById('nomeProduto_id').value = "";
+                document.getElementById('search_text').value = "";
+            }
+        }
+
         $(document).ready(function() {
             src = "{{ route('searchajax') }}";
             $("#search_text").autocomplete({
@@ -26,6 +41,11 @@
 
                         }
                     });
+                },
+                select: function (event, ui){
+                    if(ui.item){
+                        document.getElementById('nomeProduto_id').value = ui.item.value;
+                    }
                 },
                 minLength: 3,
 
